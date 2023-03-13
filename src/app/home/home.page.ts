3994b277b0f3dc8cc3ee8f9e20f25c4c29e61fe0
Juan  Hinojosa <juanma.hinojosa97@gmail.com>
2023-03-13 14:08:50 -0300

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { IonContent } from '@ionic/angular';
+import { Message } from '../models/message.model';
+import { OpenaiService } from '../services/openai.service';
+import { CustomValidators } from '../utils/custom-validators';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +11,70 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild(IonContent, {static: false}) content!: IonContent;
 
-  constructor() {}
+  messages: Message[] = [];
 
-}
+  form = new FormGroup({
+    promt: new FormControl("", [Validators.required, CustomValidators.noWhiteSpace])
+  })
+
+  loading: boolean = false;
+  constructor(
+    private openAi: OpenaiService
+  ) {}
+
+  submit() {
+    console.log(this.form.value);
+    if(this.form.valid){
+      let promt = this.form.value.promt as string;
+
+      // Mensajes Del Usuario
+      let userMsg: Message = { sender: 'me', content: promt }
+      this.messages.push(userMsg);
+
+      // Mensajes Del bot
+      let botMsg: Message = { sender: 'bot', content: '' }
+      this.messages.push(botMsg);
+
+      this.scrollToBottom()
+      this.form.reset();
+      this.form.disable();
+
+      this.loading = true;
+    
+
+      this.openAi.sendQuestion(promt).subscribe({
+        next: (res: any) => {
+          console.log(res);
+          this.loading = false;
+          this.typetext(res.bot);
+          this.form.enable();
+        }, error: (error: any) => {
+          console.log(error);
+          
+        }
+      })
+    }
+  }
+
+  typetext(text: string) {
+    let textIndex = 0;
+    let messagesLastIndex = this.messages.length -1;
+
+    let interval = setInterval(() => {
+      if(textIndex < text.length) {
+        this.messages[messagesLastIndex].content += text.charAt(textIndex);
+        textIndex++;
+      } else {
+        clearInterval(interval);
+        this.scrollToBottom();
+      }
+    }, 15)
+  }
+
+
+  scrollToBottom() {
+    this.content.scrollToBottom(2000);
+  }
+} 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+
+  baseUrl: "https://back-smart-chat.onrender.com/"
+
 };
